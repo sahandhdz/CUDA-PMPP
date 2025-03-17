08e391d2c6d2fc1eda3179b5b476c364fb5a38f2
Sahand Hadizadeh <sahand.hdz@gmail.com>
2025-03-17 11:08:15 -0500

    First successful run

--- Chapter_3/SourceCodes/img_blur/main.cu
+++ Chapter_3/SourceCodes/img_blur/main.cu
@@ -38,9 +38,40 @@ void blurKernel(unsigned char* img_input, unsigned char* img_output, int width, 
                 }
             }
         }
-        img_output[row*width*3 + col] = (unsigned char)(r_sum/pix_num);
-        img_output[row*width*3 + col + 1] = (unsigned char)(g_sum/pix_num);
-        img_output[row*width*3 + col + 2] = (unsigned char)(b_sum/pix_num);
+        img_output[row*width*3 + col*3] = (unsigned char)(r_sum/pix_num);
+        img_output[row*width*3 + col*3 + 1] = (unsigned char)(g_sum/pix_num);
+        img_output[row*width*3 + col*3 + 2] = (unsigned char)(b_sum/pix_num);
+    }
+}
+
+void blurImage(unsigned char* image_input_h, unsigned char* image_output_h, int width, int height, int blur_size){
+    int image_size = 3 * width * height * sizeof(unsigned char);;
+
+    unsigned char* image_input_d;
+    unsigned char* image_output_d;
+
+    cudaMalloc((void**)&image_input_d, image_size);
+    cudaMalloc((void**)&image_output_d, image_size);
+
+    cudaMemcpy(image_input_d, image_input_h, image_size, cudaMemcpyHostToDevice);
+
+    dim3 dimgrid(ceil(width/32.0), ceil(height/32.0), 1);
+    dim3 dimblock(32, 32, 1);
+
+    blurKernel<<<dimgrid, dimblock>>>(image_input_d, image_output_d, width, height, blur_size);
+
+    cudaMemcpy(image_output_h, image_output_d, image_size, cudaMemcpyDeviceToHost);
+
+    cudaFree(image_input_d);
+    cudaFree(image_input_h);
+}
+
+// **Save RGB Image as JPG**
+void save_rgb_image(const std::string& filename, const unsigned char* img_data, int width, int height) {
+    if (stbi_write_jpg(filename.c_str(), width, height, 3, img_data, 100)) { // Change 1 → 3 for RGB
+        std::cout << "Image saved: " << filename << std::endl;
+    } else {
+        std::cerr << "Failed to save image!" << std::endl;
     }
 }
 
@@ -56,7 +87,18 @@ int main(){
         return -1;
     }
 
+    unsigned char* img_blur = new unsigned char[width*height*3];
 
+    int blur_size = 8;
+    blurImage(img, img_blur, width, height, blur_size);
+
+    // Saving the final image.
+    std::string filename = "ladybug_Grey_blur.jpg";
+    save_rgb_image(filename, img_blur, width, height);
+
+    // Free image memory
+    stbi_image_free(img);
+    delete[] img_blur;
 
     return 0;
 }
