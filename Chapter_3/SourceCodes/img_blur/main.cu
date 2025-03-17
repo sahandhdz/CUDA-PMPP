129db121bd7721450c9d38d728523b5520efdfdd
Sahand Hadizadeh <sahand.hdz@gmail.com>
2025-03-17 02:17:21 -0500

    First draft of image blur kernel. Not yet compiled. The 3 color channels have not been considered.

---
+++ Chapter_3/SourceCodes/img_blur/main.cu
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <cstdio>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+// Including the impelemntation of stb_image.h for image loading
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+// Including the implementation of stb_image_write.h for image saving
+#define STB_IMAGE_WRITE_IMPLEMENTATION 
+#include "stb_image_write.h"
+
+#include <iostream>
+
+
+__global__
+void blurkernel(unsigned char* img_input, unsigned char* img_output, int width, int height, int blur_size){
+    // col and row indices, specifying the unqiue coordinate assigned to each thread (we use a 2D thread mapping)
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (col < width && row < height){
+        int pix_sum = 0;
+        int pix_num = 0;
+
+        for (int i=-blur_size; i<blur_size+1; i++){
+            for (int j=-blur_size; j<blur_size+1; j++){
+                int current_row = row+i;
+                int current_col = col+j;
+
+                if (current_row >=0 && current_row <height && current_col >=0 && current_col < width){
+                    pix_sum += img_input[current_row*width + current_col];
+                    pix_num ++;
+                }
+            }
+        }
+        img_output[row*width + col] = (unsigned char)(pix_sum/pix_num);
+    }
+}
+
+
+
+
+
+
+
+int main(){
+    // Reading the image using stb_image.h header by Sean T. Barrett (that's why it starts with stb!).
+    // (github.com/nothings/stb)
+    int width, height, channels;
+    unsigned char* img = stbi_load("ladybug.jpg", &width, &height, &channels, 0);
+    
+    if (!img) {
+        std::cerr << "Error loading image!" << std::endl;
+        return -1;
+    }
+
+
+
+    return 0;
+}
